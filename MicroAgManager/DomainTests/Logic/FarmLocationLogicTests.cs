f56aed19cb622cbc0f73ae88a8b63f934ebb4b44
William E Davenport <williamedavenport@hotmail.com>
2024-03-16 22:02:23 -0400

    The most significant changes in the code are related to the addition of unit tests for the `FarmLocationLogic` class. Two new test methods have been added: `OnFarmLocationCreatedTest_ValidId_ReturnsModifiedEntities` and `OnFarmLocationCreatedTest_InvalidId_ThrowsException`. These methods test the `OnFarmLocationCreated` method of the `FarmLocationLogic` class with a valid and invalid ID respectively.
    List of changes:
    
    1. Addition of unit tests for the `FarmLocationLogic` class in the `Domain.Logic.Tests` namespace. This change is aimed at ensuring the `FarmLocationLogic` class functions as expected.
    2. Addition of the `OnFarmLocationCreatedTest_ValidId_ReturnsModifiedEntities` test method. This method tests the `OnFarmLocationCreated` method of the `FarmLocationLogic` class with a valid ID, checking if the method returns the correct number of modified entities and if the first modified entity is the expected one.
    3. Addition of the `OnFarmLocationCreatedTest_InvalidId_ThrowsException` test method. This method tests the `OnFarmLocationCreated` method of the `FarmLocationLogic` class with an invalid ID, checking if the method throws an exception as expected.
    4. Addition of the `Microsoft.VisualStudio.TestTools.UnitTesting` namespace to use the `TestClass` and `TestMethod` attributes for unit testing.
    5. Addition of the `System`, `System.Collections.Generic`, `System.Linq`, `System.Text`, and `System.Threading.Tasks` namespaces to use their respective classes and methods in the test methods.
    6. Addition of the `Domain.Logic` and `DomainTests` namespaces to use the `FarmLocationLogic` class and the `TestMicroAgManagementDbContext` class respectively.

---
+++ MicroAgManager/DomainTests/Logic/FarmLocationLogicTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domain.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainTests;
+
+namespace Domain.Logic.Tests
+{
+    [TestClass()]
+    public class FarmLocationLogicTests
+    {
+        [TestMethod()]
+        public async Task OnFarmLocationCreatedTest_ValidId_ReturnsModifiedEntities()
+        {
+
+            // Arrange
+
+            var dbContext = new TestMicroAgManagementDbContext().CreateContext();
+            var Id = Guid.NewGuid();
+            var farmLocation = new Entity.FarmLocation(Id, Id) { Id = 1,Name="Test Farm"};
+            dbContext.Farms.Add(farmLocation);
+            dbContext.SaveChanges();
+
+            // Act
+            var result = await FarmLocationLogic.OnFarmLocationCreated(dbContext, 1, new CancellationToken());
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(8, result.Count);
+            Assert.AreEqual("FarmLocation", result[0].EntityName);
+            Assert.AreEqual("Created", result[0].Modification);
+        }
+
+        [TestMethod()]
+        public async Task OnFarmLocationCreatedTest_InvalidId_ThrowsException()
+        {
+            // Arrange
+            var dbContext = new TestMicroAgManagementDbContext().CreateContext();
+
+            // Act & Assert
+            await Assert.ThrowsExceptionAsync<Exception>(() => FarmLocationLogic.OnFarmLocationCreated(dbContext, 1, new CancellationToken()));
+        }
+
+    }
+}
